87ad7fd214d8de7914de32d762e3fd953eaaa666
Jetoxixoxorix <technoamfa13@gmail.com>
2019-04-18 20:13:46 +0200

    working getting array with promises

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,16 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
+import axios from 'axios';
+
+export const testingAxios = () => {
+
+    const users = axios.get("https://jsonplaceholder.typicode.com/users/")
+                            .then(response =>  response.data)
+        
+    return users;
+}
+
+//sprobowac do component did mount jak w przykladzie z alligatora i zobaczyc cyz to dziala
 export class HomeContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: []
+            user: [],
+            users: []
         }
     }
 
+    handleClick = async () => {
+        const axios = await testingAxios();
+        console.log(axios);
+    }
+
     render() {
-        return(
-            <h1>Hello {this.state.user.username}</h1>
+        return (
+            <div>
+                <h1>Hello {this.state.user.username}</h1>
+                <input type="submit" onClick={this.handleClick} value="elo"/>
+            </div>
         )
     }
 }
